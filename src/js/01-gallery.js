0327d528b767937b8c695ac6d8397f8df14bd30f
Vlad Shestavetskyi <vladislavshsh@gmail.com>
2022-07-24 16:24:28 +0300

    k

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "../css/01-gallery.css"
+
+
 
 console.log(galleryItems);
+
+
+
+
+const gallery = document.querySelector(".gallery");
+
+
+
+
+
+
+const imgGallery = galleryItems.map(({ preview, original, description }) => {
+    console.log(galleryItems);
+    return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    >
+  </a>`
+}).join(" ");
+gallery.insertAdjacentHTML("beforeend", `${imgGallery}`);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
